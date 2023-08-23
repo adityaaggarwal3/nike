2e4af84bc9143b50fa46267d973bfda31abd4c55
Aditya <aditya.aggarwal@live.com>
2023-08-23 09:55:30 +0530

    Git initialisation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-center bg-slate-400 py-4">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
