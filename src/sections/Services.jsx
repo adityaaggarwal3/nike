e00e73c6ead10989d477fbb9f0a1dad0cc36f588
Aditya <aditya.aggarwal@live.com>
2023-08-25 05:51:09 +0530

    Final Commit

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -4,7 +4,7 @@ import { services } from "../constants";
 const Services = () => {
   // console.log(services);
   return (
-    <section className="flex flex-1 justify-between items-center gap-5 max-container my-10 p-5">
+    <section className="max-container flex justify-center flex-wrap gap-9">
       {services.map((service, index) => {
         return <ServiceCard key={index} {...service} />;
       })}
